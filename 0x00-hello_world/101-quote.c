cfb92e5191815c4db74596a3c8b7d7b82b0d0464
Alphonce K. Mutai <mutaikalfonce@gmail.com>
2023-02-19 04:18:54 -0800

    A program that prints to a standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - program that prints exactly to the standard error
+ * main- printing  exactly to the standard error
  * Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
