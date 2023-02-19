0df6b99e6a72d384265e9ef006e9930fa6c838a0
Alphonce K. Mutai <mutaikalfonce@gmail.com>
2023-02-18 23:52:16 -0800

    printing a line using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Write a C program using puts function
+ * using the puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts(\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
