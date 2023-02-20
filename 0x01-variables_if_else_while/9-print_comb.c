0bf5d6c7c99f0de83769201ec9197fd5ccc073f9
Alphonce K. Mutai <mutaikalfonce@gmail.com>
2023-02-20 14:11:11 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,16 +5,18 @@
  */
 int main(void)
 {
-	int a;
+	int c = 0;
 
-	for(a = 0; a < 10 ; a++);
-}
-		putchar(a + '0');
-		if (a < 9)
-{
-	putchar(',');
-	putchar(' ');
-}
-putchar('\n');
-return (0);
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
