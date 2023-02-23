326c2c376d3d607fd70e5c79eb8d6dde35486d3e
Alphonce K. Mutai <mutaikalfonce@gmail.com>
2023-02-23 12:48:17 -0800

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,23 +1,24 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
- * main causes an infinite loop
- *  Return: 0 
- */
-int main(void) 
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
 {
-	int i;
+        int i;
 
-	printf("Infinite Toop incoming :\n");
+        printf("Infinite loop incoming :(\n");
 
-       	i = 0;
-	/*
-	 * while (i < 10)
-	 *{
-	 *                          putchar(i);
-	 *}	 
-	 */
-printf("Infinite Toop avoided! \\o/\n");
+        i = 0;
 
-return (0);
+        while (i < 10)
+        {
+                putchar(i);
+        }
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
 }
