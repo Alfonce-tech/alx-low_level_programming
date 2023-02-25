4facec8a284a2cf9cef24dba85c365a1c988f7d6
Alphonce K. Mutai <mutaikalfonce@gmail.com>
2023-02-25 00:30:46 -0800

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * times_table -prints the 9 times table,starting with 0
- * Return: empty output
+ *times_table -prints the 9 times table,starting with 0
+ *Return: empty output
  */
 void times_table(void)
 {
