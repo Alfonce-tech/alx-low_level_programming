53031ec5871c1050fee7598a1a2c51373af215e4
Alphonce K. Mutai <mutaikalfonce@gmail.com>
2023-02-28 01:40:38 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _strlen - Returns the length of a string
+ * @str: the string to the length of
+ * Return: the length of @str.
+ */
+int _strlen(char *s)
+{
+	size_t length = 0;
+
+	while (*str++)
+	length++;
+	return (length);
+}
